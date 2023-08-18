326e804f0196bc4698fbaa8dcee793657d468d64
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-08-18 20:55:35 +0100

    update python-app.yml

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages# List of requirements
 requirements = []  # This could be retrieved from requirements.txt# Package (minimal) configuration
 setup(
-    name="coding challanges",
+    name="challanges",
     version="1.0.0",
     description="leetcode challanges in python ",
     packages=find_packages(),  # __init__.py folders search
