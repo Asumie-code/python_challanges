1b65daff37557193ecbfd99f28a4fc12b748eac6
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-09-18 20:02:44 +0100

    challange: long_prefix

--- com/challanges/main.py
+++ com/challanges/main.py
@@ -1,44 +1,40 @@
-
-
-
-
-def roman_to_int(s): 
+def roman_to_int(s):
     roman_num = s
     sum = 0
-    roman = {
-        'I': 1, 
-        'V': 5, 
-        'X': 10, 
-        'L': 50, 
-        'C': 100, 
-        'D': 500, 
-        'M': 1000
-    }
-    
-    if 'IX' in roman_num :
-            roman_num = roman_num.replace('IX', '')
-            sum += 9
-    if 'IV' in roman_num :
-            roman_num = roman_num.replace('IV', '')
-            sum += 4 
-    if 'XL' in roman_num :
-            roman_num = roman_num.replace('XL', '')
-            sum += 40 
-    if 'XC' in roman_num :
-            roman_num = roman_num.replace('XC', '')
-            sum += 90
-    if 'CD' in roman_num :
-            roman_num = roman_num.replace('CD', '')
-            sum += 400 
-    if 'CM' in roman_num: 
-            roman_num = roman_num.replace('CM', '')
-            sum += 900 
-        
-    for c in roman_num: 
-           sum += roman[c]
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    if "IX" in roman_num:
+        roman_num = roman_num.replace("IX", "")
+        sum += 9
+    if "IV" in roman_num:
+        roman_num = roman_num.replace("IV", "")
+        sum += 4
+    if "XL" in roman_num:
+        roman_num = roman_num.replace("XL", "")
+        sum += 40
+    if "XC" in roman_num:
+        roman_num = roman_num.replace("XC", "")
+        sum += 90
+    if "CD" in roman_num:
+        roman_num = roman_num.replace("CD", "")
+        sum += 400
+    if "CM" in roman_num:
+        roman_num = roman_num.replace("CM", "")
+        sum += 900
+
+    for c in roman_num:
+        sum += roman[c]
 
     return sum
- 
 
 
-
+def long_prefix(v):
+        ans=""
+        v=sorted(v)
+        first=v[0]
+        last=v[-1]
+        for i in range(min(len(first),len(last))):
+            if(first[i]!=last[i]):
+                return ans
+            ans+=first[i]
+        return ans 
--- tests/test_main.py
+++ tests/test_main.py
@@ -2,8 +2,12 @@ import com.challanges.main as challanges
 
 
 
-def test_func(): 
+def test_roman_to_int(): 
     assert challanges.roman_to_int('LVIII') == 58
     assert challanges.roman_to_int('MCMXCIV') == 1994
     assert challanges.roman_to_int('III') == 3
     
+
+def test_long_prefix(): 
+    assert challanges.long_prefix(["flower","flow","flight"]) =="fl"
+    assert challanges.long_prefix(["dog","racecar","car"]) ==""
