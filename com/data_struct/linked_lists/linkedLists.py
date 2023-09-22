eb1a5e73bd40bf4358d5986093129e51d7d967c4
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-09-22 19:48:14 +0100

    feat: implement linkedLists:constructor

--- com/data_struct/linked_lists/linkedLists.py
+++ com/data_struct/linked_lists/linkedLists.py
@@ -3,3 +3,20 @@ class Node:
         self.value = val
         self.next = None
         self.prev = None
+
+class LinkedList: 
+    def __init__(self, *values):
+        self._length = 0
+        self._head = self._tail = None
+    
+    @property
+    def head(self): 
+        return self._head
+    
+    @property
+    def tail(self): 
+        return self._tail
+    
+    @property
+    def length(self):
+        return self._length
