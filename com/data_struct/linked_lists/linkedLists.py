b241c6d565e71a858114260910c13c92eb39b762
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-09-28 10:33:42 +0100

    feat: linkedLists
      - implement insert, remove, removeHeade, removeTail

--- com/data_struct/linked_lists/linkedLists.py
+++ com/data_struct/linked_lists/linkedLists.py
@@ -64,7 +64,97 @@ class LinkedList:
 
         self._length += 1
         return True
+    
+    def insert(self, val, previousItem, checkDuplicates = False): 
+        if checkDuplicates and self._isDuplicate(val): 
+            return False
+        newItem = Node(val)
+        currentItem = self._head
 
+        if not currentItem: 
+            return False
+        else: 
+            while True: 
+                if currentItem.value == previousItem: 
+                    newItem.next = currentItem.next 
+                    newItem.prev = currentItem
+                    currentItem.next = newItem
+
+                    if newItem.next: 
+                        newItem.next.prev = newItem
+                    else: 
+                        self._tail = newItem
+
+                    self._length += 1
+                    return True
+                else: 
+                    if currentItem.next: 
+                        currentItem = currentItem.next
+                    else: 
+                        return False
+            
+    def remove(self, val): 
+        currentItem = self._head
+
+        if not currentItem: return
+
+        if currentItem.value == val: 
+            self._head = currentItem.next
+            self._head.prev = None
+            currentItem.next = currentItem.prev = None
+            self._length -=1 
+            return currentItem.value
+        else: 
+            while True: 
+                if currentItem.value == val: 
+                    if currentItem.next:
+                        currentItem.prev.next = currentItem.next
+                        currentItem.next.prev = currentItem.prev
+                        currentItem.next = currentItem.prev = None
+                    else: 
+                        currentItem.prev.next = None
+                        self._tail = currentItem.prev
+                        currentItem.next = currentItem.prev = None
+                    
+                    self._length -= 1
+                    return currentItem.value
+                else: 
+                    if currentItem.next: currentItem = currentItem.next
+                    else: return
+
+
+    def removeHead(self): 
+        currentItem = self._head
+
+        if not currentItem: return 
+
+        if not self._head.next: 
+            self._head = None 
+            self._tail = None
+        else: 
+            self._head.next.prev = None 
+            self._head = self._head.next 
+            currentItem.next = currentItem.prev = None
+        
+        self._length -= 1
+        return currentItem.value
+    
+
+    def removeTail(self):
+        currentItem = self._tail
+
+        if not currentItem: return
+
+        if not self._tail.prev: 
+            self._head = None 
+            self._tail = None
+        else: 
+            self._tail.prev.next = None 
+            self._tail = self._tail.prev 
+            currentItem.next = currentItem.prev = None
+        
+        self._length -= 1 
+        return currentItem.value
 
     def toList(self): 
         return [*self]
