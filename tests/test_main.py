2dbc07313513c09a17fe852b3f00d5b7fd849c1a
Asumie-code <darkeangel.1998.ia@gmail.com>
2023-09-29 19:00:07 +0100

    challanges: mergeTwoLists

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,3 +17,30 @@ def test_long_prefix():
 def test_remove_duplicates(): 
     assert challanges.remove_duplicates([1,1,2,2,3,3])  == 3
     assert challanges.remove_duplicates([1,1,2,2,3,3,5,5])  == 4
+
+
+def test_mergeTwoLists(): 
+    lis1 = [1,2,4]
+    lis2 = [1,3,5]
+    result = lis1 + lis2
+    result.sort()
+    current = dummylis1 = challanges.ListNode()
+    for i in lis1: 
+        current.next = challanges.ListNode(i)
+        current = current.next
+    lis_node_1 = dummylis1.next
+
+
+    current = dummylis2 = challanges.ListNode()
+    for i in lis2: 
+        current.next = challanges.ListNode(i)
+        current = current.next
+    lis_node_2 = dummylis2.next
+
+
+    lis_node_result = challanges.mergeTwoLists(lis_node_1, lis_node_2) 
+    current = lis_node_result 
+    for i in result: 
+        if current:
+            assert current.val == i 
+            current = current.next
