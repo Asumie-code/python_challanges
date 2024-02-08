b1f2d49ee0764a1e4ee0e1ae20682b8dde651ffc
asumie-code <darkeangel.1998.ia@gmail.com>
2024-02-08 11:21:09 +0100

    challanges: queues using stack

--- com/challanges/main.py
+++ com/challanges/main.py
@@ -675,4 +675,25 @@ def summaryRanges(nums: List[int]) -> List[str]:
 
 def isPowerOfTwo(n: int) -> bool: 
     return (n and (not(n & (n - 1))))
+
+
+class MyQueue: 
+    def __init__(self): 
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x) -> None: 
+        while self.s2: 
+            self.s1.append(self.s2.pop())
+        self.s1.append(x)
+        while self.s1: 
+            self.s2.append(self.s1.pop())
+    
+    def pop(self) -> int: 
+        return self.s2.pop()
+
+    def peek(self) -> int: 
+        return self.s2[-1]
+    def empty(self) -> bool: 
+        return not bool(self.s2)
     
